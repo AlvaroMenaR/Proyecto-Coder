1e8de4a4dcae1b747eb2f9183f66f311e8dde143
Alvaro Mena <alvaromenar@gmail.com>
2024-04-19 19:09:58 -0400

    Probando funcionalidad de ajax 2

--- js/form.js
+++ js/form.js
@@ -1,22 +1,43 @@
+//Codigo AJAX
+
 $('form.ajax').submit(function(evento){
     evento.preventDefault();
         console.log(evento);
         console.log($(this));
+
+let objectForm = $(this),
+sendMethod = objectForm.attr('method'),
+sendUrl = objectForm.attr('action'),
+sendData = {};
+
+objectForm.find("[name]").each(function(index, formItem){
+console.log(index, formItem);
+
+let item = $(this),
+name = item.attr('name'),
+nameValue = item.val();
+
+sendData[name] = nameValue;
+
 });
 
-
-
+console.log(sendData);
 
 
 $.ajax({
-    method: 'POST',
-    url: 'https://formsubmit.co/ajax/alvaromenar@gmail.com',
+    method: sendMethod,
+    url: sendUrl,
     dataType: 'json',
     accepts: 'application/json',
-    data: {
-        name: "FormSubmit",
-        message: "I'm from Devro LABS"
-    },
+    data: sendData,
     success: (data) => console.log(data),
     error: (err) => console.log(err)
 });
+
+
+});
+
+
+
+
+
